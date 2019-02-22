1e69a9b1811787f7d726b8618ece3debb03c6e04
Shane Dooley <dooley.shanek@gmail.com>
2019-02-21 19:12:27 -0600

    Fixed log and ppois instead of dpois

--- BCB_2_ComputationalStats/HW1/Q2_DPBinomial.R
+++ BCB_2_ComputationalStats/HW1/Q2_DPBinomial.R
@@ -3,34 +3,60 @@
 
 #Q2b.
 dpbinom = function(x, prob, log = FALSE, method = c("MC", "PA", "NA", "BA"), nsim = 1e4) {
-  prob = 10^(-(prob-33)/10)
-  #print(prob)
+  prob = 1-10^(-(prob-33)/10)
   stopifnot(all(prob >= 0 & prob <= 1))
   method <- match.arg(method)
-  
   if (method == "PA") {
     # poisson
-    ppois(x, sum(prob), log)
+    ppois(x, sum(prob), log.p=log)
   } else if (method == "NA") {
     # normal
-    pnorm(x, sum(prob), sqrt(sum(prob*(1-prob))), log)
+    pnorm(x, sum(prob), sqrt(sum(prob*(1-prob))), log.p=log)
   } else if (method == "BA") {
     # binomial
-    pbinom(x, length(prob), mean(prob), log)
+    pbinom(x, length(prob), mean(prob), log.p=log)
   } else {
-    
     # monte carlo
     tmp <- table(colSums(replicate(nsim, rbinom(length(prob), 1, prob))))
     tmp <- tmp/sum(tmp)
     p <- as.numeric(tmp[as.character(x)])
     p[is.na(p)] <- 0
-    
     if (log) log(p)
     else p 
   }
 }
 
 
+
+
+
+d768 = read.table("data/I0_Pos768_data.txt", header=T)
+d962 = read.table("data/Pos962_data.txt", header=T)
+
+dpbinom(8869,d768$q,method="PA", log = FALSE) #Homo -> close to 1
+1-ppois(8869,8865.613,FALSE)
+dpbinom(4827,d962$q,method="PA", log = FALSE) #het -> close to 0
+ppois(4827,8865.006)
+
+
+d768$e = 1- 10^(-(d768$q-33)/10)
+d962$e = 1- 10^(-(d962$q-33)/10)
+
+sum(d768$e)
+sum(d962$e)
+
+ppois(8869, lambda=sum(d768$e),log=FALSE)
+ppois(4827, sum(d962$e),log=FALSE )
+
+
+
+
+
+
+
+
+
+pnorm(x, sum(prob), sqrt(sum(prob*(1-prob))), log)
 
 
 ####################### Development Artifacts below here #######################
@@ -54,3 +80,4 @@ dpbinom = function(x, prob, log = FALSE, method = c("MC", "PA", "NA", "BA"), nsi
 #   }
 #   sum/nsims
 # }
+help(ppois)
