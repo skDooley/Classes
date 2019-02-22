98840bea1b4cca098b67a287cacba08e7535f6b8
dooleys1 <dooley@iastate.edu>
2019-02-21 21:07:29 -0500

    Final alterations to code, the rest is just to make it pretty

--- BCB_2_ComputationalStats/HW1/Q2_DPBinomial.R
+++ BCB_2_ComputationalStats/HW1/Q2_DPBinomial.R
@@ -30,33 +30,26 @@ dpbinom = function(x, prob, log = FALSE, method = c("MC", "PA", "NA", "BA"), nsi
 
 
 
-d768 = read.table("data/I0_Pos768_data.txt", header=T)
-d962 = read.table("data/Pos962_data.txt", header=T)
+# d768 = read.table("data/I0_Pos768_data.txt", header=T)
+# d962 = read.table("data/Pos962_data.txt", header=T)
 
-dpbinom(8869,d768$q,method="PA", log = FALSE) #Homo -> close to 1
-1-ppois(8869,8865.613,FALSE)
-dpbinom(4827,d962$q,method="PA", log = FALSE) #het -> close to 0
-ppois(4827,8865.006)
-
-
-d768$e = 1- 10^(-(d768$q-33)/10)
-d962$e = 1- 10^(-(d962$q-33)/10)
-
-sum(d768$e)
-sum(d962$e)
-
-ppois(8869, lambda=sum(d768$e),log=FALSE)
-ppois(4827, sum(d962$e),log=FALSE )
+# dpbinom(8869,d768$q,method="PA", log = FALSE) #Homo -> close to 1
+# 1-ppois(8869,8865.613,FALSE)
+# dpbinom(4827,d962$q,method="PA", log = FALSE) #het -> close to 0
+# ppois(4827,8865.006)
 
 
+# d768$e = 1- 10^(-(d768$q-33)/10)
+# d962$e = 1- 10^(-(d962$q-33)/10)
+
+# sum(d768$e)
+# sum(d962$e)
+
+# ppois(8869, lambda=sum(d768$e),log=FALSE)
+# ppois(4827, sum(d962$e),log=FALSE )
 
 
 
-
-
-
-
-pnorm(x, sum(prob), sqrt(sum(prob*(1-prob))), log)
 
 
 ####################### Development Artifacts below here #######################
